1a80f1b2e4b6e71c03dd01ac77e6667ab69218fe
Alba Matamoros Morales <albamamo07@gmail.com>
2024-10-07 16:35:22 +0200

    Añadir botones inicio

---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <!-- Alba Matamoros Morales -->
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="estilIndex.css">
+    <title>Inici</title>
+</head>
+<body>
+    <nav>
+        <div class="perfil">
+            <button type="button">PERFIL</button>
+            <div class="dropdown-content">
+                <button type="button" onclick="location.href='./perfil.iniciar.php'">Iniciar sesión</button>
+                <button type="button" onclick="location.href='./menu.vista.php'">Registrarse</button>
+            </div>
+        </div>
+        <div>
+            <button type="button" onclick="location.href='./index.php'">INICI</button>
+            <button type="button" onclick="location.href='./menu.vista.php'">GESTIÓ D'ARTICLES</button>
+        </div>
+    </nav>
+</body>
+</html>
