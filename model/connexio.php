70bf1efffbddd11a694cbdf5226bb75766ad35d3
Alba Matamoros Morales <albamamo07@gmail.com>
2024-10-09 20:03:52 +0200

    Connexio

---
+++ model/connexio.php
@@ -0,0 +1,16 @@
+//CONNEXIO
+function connexio(){
+    //Dades connexio a BD.
+    $host = "localhost";
+    $nomBD = "pt02_alba_matamoros";
+    $usuari = "root";
+    $contra = "";
+
+    //Connexió.
+    try {
+       return new PDO("mysql:host=$host;dbname=$nomBD", $usuari, $contra);
+        echo "Connexio correcta!!" . "<br />"; 
+    } catch (PDOException $e){
+        die("Error: " . $e->getMessage());
+    }
+}
